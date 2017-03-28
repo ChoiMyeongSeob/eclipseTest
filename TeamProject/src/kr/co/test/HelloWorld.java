f6a1557983bf3f9f3d5f5a8d52440834690068f6
MS <mams12@naver.com>
2017-03-28 10:39:40 +0900

    tpasdlfkj

--- TeamProject/src/kr/co/test/HelloWorld.java
+++ TeamProject/src/kr/co/test/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World~~~!@~!@~!@~!@~!@~@~");
 		System.out.println("두번째냐냐냐냐냐냐냐냐냐냐");
-		System.out.println("세컨드다다다다다다다다다다다");
+		System.out.println("asdfasdfsadfasd");
+		
 	}
 }
